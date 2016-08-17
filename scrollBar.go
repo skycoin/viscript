9555c80c490d6d61ed249983343d929cc4b18ded
corpusc <corpus_callosum@hotmail.com>
2016-08-17 18:44:33 -0400

    ... it's storming

--- scrollBar.go
+++ scrollBar.go
@@ -4,19 +4,25 @@ import (
 //"fmt"
 )
 
-var scrollPosX float32
-var scrollPosY float32 = rectRad
+var sb = scrollBar{0, rectRad, 0, 0}
+
+type scrollBar struct {
+	scrollPosX    float32
+	scrollPosY    float32
+	scrollBarLen  float32
+	scrollVoidLen float32
+}
 
 func scroll(mousePixelDeltaY float64) {
-	scrollPosY -= float32(mousePixelDeltaY) * pixelHei
+	sb.scrollPosY -= float32(mousePixelDeltaY) * pixelHei
 
-	if scrollPosY < -rectRad+scrollBarLen {
-		scrollPosY = -rectRad + scrollBarLen
+	if sb.scrollPosY < -rectRad+scrollBarLen {
+		sb.scrollPosY = -rectRad + scrollBarLen
 	}
-	if scrollPosY > rectRad { // (rectRad-scrollBarLen)
-		scrollPosY = rectRad
+	if sb.scrollPosY > rectRad {
+		sb.scrollPosY = rectRad
 	}
 
-	viewportOffsetY = scrollPosY / scrollVoidLen * float32(len(document)) * chHei
+	viewportOffsetY = sb.scrollPosY / sb.scrollVoidLen * float32(len(document)) * chHei
 	fmt.Printf("scrollPosY: %.1f - viewportOffsetY: %.1f", scrollPosY, viewportOffsetY)
 }
