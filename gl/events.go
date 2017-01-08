40026b2419ea85d99b8d3d5f11888a2811a8bee2
haltingstate <haltingstate>
2017-01-08 21:29:27 +0800

    input events

---
+++ gl/events.go
@@ -0,0 +1,30 @@
+package gl
+
+import (
+	"fmt"
+	//"log"
+
+	//"bytes"
+	//"math"
+	//"strconv"
+
+	//"encoding/binary"
+
+	"github.com/corpusc/viscript/gfx"
+	//"github.com/corpusc/viscript/msg"
+	//"github.com/corpusc/viscript/script"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+func InitMiscEvents(w *glfw.Window) {
+	w.SetFramebufferSizeCallback(onFramebufferSize)
+}
+
+//direct, not wrapped
+//NOT AN INPUT EVENT
+func onFramebufferSize(w *glfw.Window, width, height int) {
+	fmt.Printf("onFramebufferSize() - width, height: %d, %d\n", width, height)
+	gfx.CurrAppWidth = int32(width)
+	gfx.CurrAppHeight = int32(height)
+	gfx.Rend.SetSize()
+}
