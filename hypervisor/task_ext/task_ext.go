0158f91ede7ba1abfd0514baf3bc6bb45261756b
redpix <dzavashviligeorge@gmail.com>
2017-04-09 21:30:51 +0400

    start and attach
    - Removed ProcessQuit because it's not necessary there's a flag
    - Renamed runningInBg to detached to indicate if the external process is detached
    - Changed routines
    - Added startRoutines method
    - Changed processInput and processOutput to using select

--- hypervisor/task_ext/task_ext.go
+++ hypervisor/task_ext/task_ext.go
@@ -2,7 +2,6 @@ package task_ext
 
 import (
 	"errors"
-	"fmt"
 
 	"io"
 
@@ -26,8 +25,6 @@ type ExternalProcess struct {
 	ProcessOut  chan []byte
 	ProcessExit chan bool // if process needs to exit without user interruption
 
-	ProcessQuit chan bool // when process should end upon user's command
-
 	CmdOut chan []byte
 	CmdIn  chan []byte
 
@@ -35,11 +32,11 @@ type ExternalProcess struct {
 	stdOutPipe io.ReadCloser
 	stdInPipe  io.WriteCloser
 
-	runningInBg bool
+	detached bool
 }
 
 //non-instanced
-func MakeNewTaskExternal(tokens []string) (*ExternalProcess, error) {
+func MakeNewTaskExternal(tokens []string, detached bool) (*ExternalProcess, error) {
 	app.At(te, "MakeNewTaskExternal")
 	var p ExternalProcess
 
@@ -48,6 +45,7 @@ func MakeNewTaskExternal(tokens []string) (*ExternalProcess, error) {
 		return nil, err
 	}
 
+	p.detached = detached
 	return &p, nil
 }
 
@@ -83,10 +81,13 @@ func (pr *ExternalProcess) Init(tokens []string) error {
 	pr.ProcessIn = make(chan []byte, 2048)
 	pr.ProcessOut = make(chan []byte, 2048)
 	pr.ProcessExit = make(chan bool)
+<<<<<<< refs/remotes/corpusc/master:hypervisor/task_ext/task_ext.go
 	pr.ProcessQuit = make(chan bool)
 
 	//TODO: maybe passed as an arg on the start?
 	pr.runningInBg = true
+=======
+>>>>>>> start and attach:hypervisor/extprocess/task_ext.go
 
 	return nil
 }
@@ -110,38 +111,31 @@ func (pr *ExternalProcess) cmdInRoutine() {
 
 	for {
 
-		select {
-		case shouldQuit := <-pr.ProcessQuit:
-			if shouldQuit == true {
-				// In this case process should:
-				// 1) Send true to the ProcessShouldEnd channel
-				// 2) Call external process's TearDown
-				// 3) Remove it from the GlobalList and further cleanup whatever will be needed
-				return
-			}
+		if pr.detached {
+			return
 		}
 
-		if !pr.runningInBg {
-			if pr.stdOutPipe == nil {
-				println("!!! Standard output pipe is nil. Sending Exit Request !!!")
-				pr.ProcessExit <- true
-				return
-			}
-			buf := make([]byte, 2048)
-
-			size, err := pr.stdOutPipe.Read(buf)
-			if err != nil {
-				f := err.Error()
-				s := fmt.Sprintf("**** ERROR! **** From \"%s\".  Returning. %s", pr.CommandLine, f)
-				for i := 0; i < 5; i++ {
-					println(s) //OS box print
-				}
-				pr.ProcessExit <- true
-				return
-			}
-			println("--- Received input to write to the terminal:", string(buf[:size]))
-			pr.CmdIn <- buf[:size]
+		if pr.stdOutPipe == nil {
+			println("!!! Standard output pipe is nil. Sending Exit Request !!!")
+			pr.ProcessExit <- true
+			return
 		}
+		buf := make([]byte, 2048)
+
+		size, err := pr.stdOutPipe.Read(buf)
+		if err != nil {
+			// f := err.Error()
+			// s := fmt.Sprintf("**** ERROR! **** From \"%s\".  Returning. %s", pr.CommandLine, f)
+			// for i := 0; i < 5; i++ {
+			// 	println(s) //OS box print
+			// }
+			pr.ProcessExit <- true
+			return
+		}
+
+		println("--- Received input to write to the terminal:", string(buf[:size]))
+		pr.CmdIn <- buf[:size]
+
 	}
 }
 
@@ -149,43 +143,50 @@ func (pr *ExternalProcess) cmdOutRoutine() {
 	app.At(te, "cmdOutRoutine")
 
 	for {
+		if pr.detached {
+			return
+		}
+
 		select {
-		case shouldEnd := <-pr.ProcessQuit:
-			if shouldEnd == true {
+		case data := <-pr.CmdOut:
+
+			if pr.stdInPipe == nil {
+				println("!!! Standard input pipe is nil. Sending Exit Request !!!")
+				pr.ProcessExit <- true
 				return
 			}
-		case data := <-pr.CmdOut:
-			if !pr.runningInBg {
 
-				if pr.stdInPipe == nil {
-					println("!!! Standard input pipe is nil. Sending Exit Request !!!")
-					pr.ProcessExit <- true
-					return
-				}
-
-				println("--- Received input to write to external process:", string(data))
-				_, err := pr.stdInPipe.Write(append(data, '\n'))
-				if err != nil {
-					pr.ProcessExit <- true
-					return
-				}
+			println("--- Received input to write to external process:", string(data))
+			_, err := pr.stdInPipe.Write(append(data, '\n'))
+			if err != nil {
+				pr.ProcessExit <- true
+				return
 			}
 		}
 	}
 }
 
-func (pr *ExternalProcess) ProcessOutput() {
-	for len(pr.ProcessIn) > 0 {
-		// println("ProcessOutput() - data := ", string(data))
-		data := <-pr.ProcessIn
+func (pr *ExternalProcess) startRoutines() {
+	// Run the routine which will read and send the data to CmdIn
+	go pr.cmdInRoutine()
+	// Run the routine which will read from Cmdout and write to process
+	go pr.cmdOutRoutine()
+}
+
+func (pr *ExternalProcess) processOutput() {
+	select {
+	case data := <-pr.ProcessIn:
+		println("ProcessOutput() - data := ", string(data))
 		pr.CmdOut <- data
+	default:
 	}
 }
 
-func (pr *ExternalProcess) ProcessInput() {
-	for len(pr.CmdIn) > 0 {
-		// println("ProcessInput() - data := ", string(data))
-		data := <-pr.CmdIn
+func (pr *ExternalProcess) processInput() {
+	select {
+	case data := <-pr.CmdIn:
+		println("ProcessInput() - data := ", string(data))
 		pr.ProcessOut <- data
+	default:
 	}
 }
