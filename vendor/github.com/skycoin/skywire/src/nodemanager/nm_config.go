0f81c39eb7352e4ab34f015df02762791e868d2f
gz-c <gz-c@users.noreply.github.com>
2017-06-28 13:01:49 +0800

    Update vendor to remove skywire

--- vendor/github.com/skycoin/skywire/src/nodemanager/nm_config.go
+++
@@ -1,9 +0,0 @@
-package nodemanager
-
-type NodeManagerConfig struct {
-	Domain           string // domain name of the nodemanager
-	CtrlAddr         string // address for talking with nodes
-	AppTrackerAddr   string // address of service manager
-	RouteManagerAddr string // address of route finding service
-	LogisticsServer  string // address of logistics service
-}
