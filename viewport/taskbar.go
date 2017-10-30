5a8772f8b7c64864d4933265f5bda5deb5615fda
corpusc <corpus_callosum@hotmail.com>
2017-10-29 23:39:26 -0400

    + full text on terminal buttons

--- viewport/taskbar.go
+++ viewport/taskbar.go
@@ -10,7 +10,7 @@ var (
 	taskBarDepth      float32 = 11
 	taskBarHeight     float32 = 0.1
 	taskBarBorderSpan float32 = taskBarHeight / 12
-	taskBarCurrX      float32
+	taskBarCharWid    float32 = taskBarHeight/2 - taskBarBorderSpan*2
 	buttonBounds      *app.Rectangle
 	charBounds        *app.Rectangle
 )
@@ -36,12 +36,11 @@ func drawTaskBarBackground() {
 }
 
 func drawStartButton() {
+	//now make buttons inset from task bar
 	buttonBounds.Top -= taskBarBorderSpan
 	buttonBounds.Bottom += taskBarBorderSpan
 	buttonBounds.Left += taskBarBorderSpan
 	buttonBounds.Right = buttonBounds.Left + buttonBounds.Height()
-
-	taskBarCurrX = buttonBounds.Right
 
 	gl.Draw9SlicedRect(
 		gl.Pic_GradientBorder,
@@ -54,22 +53,22 @@ func drawStartButton() {
 		buttonBounds.Bottom + taskBarBorderSpan,
 		buttonBounds.Left + taskBarBorderSpan}
 
-	buttonBounds.Left += buttonBounds.Height()
-	buttonBounds.Right += buttonBounds.Height()
+	buttonBounds.Left += buttonBounds.Width()
+	buttonBounds.Right += buttonBounds.Width()
 
 	gl.Draw9SlicedRect(
 		gl.Pic_TriangleUp,
 		charBounds,
 		taskBarDepth)
 
-	charBounds.Left += charBounds.Height()
-	charBounds.Right += charBounds.Height()
+	charBounds.Left += taskBarCharWid
+	charBounds.Right += taskBarCharWid
 }
 
 func drawTerminalButtons() {
 	for _, term := range terminal.Terms.TermMap {
-		charBounds.Left += taskBarBorderSpan * 10  //2
-		charBounds.Right += taskBarBorderSpan * 10 //2
+		charBounds.Left = buttonBounds.Left + taskBarBorderSpan
+		charBounds.Right = charBounds.Left + taskBarCharWid
 
 		if term.TerminalId == terminal.Terms.FocusedId {
 			gl.SetColor(gl.White)
@@ -78,7 +77,7 @@ func drawTerminalButtons() {
 		}
 
 		max := len(term.TabText)
-		currTextWid := charBounds.Width()*float32(max) + taskBarBorderSpan*2
+		currTextWid := taskBarCharWid*float32(max) + taskBarBorderSpan*2
 		buttonBounds.Right = buttonBounds.Left + currTextWid
 
 		//draw button background
@@ -88,11 +87,11 @@ func drawTerminalButtons() {
 			taskBarDepth)
 
 		//draw the id # text
-		// for i := 0; i < max; i++ {
-		// 	gl.DrawCharAtRect(rune(term.TabText[i]), charBounds, taskBarDepth)
-		// 	charBounds.Left += term.CharSize.X
-		// 	charBounds.Right += term.CharSize.X
-		// }
+		for i := 0; i < max; i++ {
+			gl.DrawCharAtRect(rune(term.TabText[i]), charBounds, taskBarDepth)
+			charBounds.Left += taskBarCharWid
+			charBounds.Right += taskBarCharWid
+		}
 
 		buttonBounds.Left += currTextWid
 		buttonBounds.Right += currTextWid
